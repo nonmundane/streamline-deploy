192f15c2a21a4d14d0e4293db1d55cc051113151
pookie <pookiebear@pookies-MacBook-Pro.local>
2020-06-03 19:23:49 -0700

    minor fixes

--- movienight/video-hls.js
+++ movienight/video-hls.js
@@ -3,13 +3,13 @@
 function initPlayer() {
     if (navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) {
         var videoElement = document.getElementById("videoElement");
-        videoElement.src = "http://HOSTNAME.DOMAINNAME/master.m3u8";
+        videoElement.src = "https://HOSTNAME.DOMAINNAME/master.m3u8";
         videoElement.autoplay = true;
     }
     else if(Hls.isSupported()) {
         var videoElement = document.getElementById("videoElement");
         var hls = new Hls();
-        hls.loadSource("http://HOSTNAME.DOMAINNAME/master.m3u8");
+        hls.loadSource("https://HOSTNAME.DOMAINNAME/master.m3u8");
         hls.attachMedia(videoElement);
         hls.on(Hls.Events.MANIFEST_PARSED,function() {
           videoElement.play();
