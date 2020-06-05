7812dc615b9b1b434cd1b417ef1d5ab9a456d83e
pookie <pookiebear@pookies-MacBook-Pro.local>
2020-06-04 18:19:38 -0700

    reordering

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "hostname" {
-  type    = string
-  default = "streamii"
-}
-variable "instance_name" {
-  type = string
-  default = "streamlineinstance"
-}
 variable "domainname" {
   type    = string
   default = "pookflix.com"
@@ -14,33 +6,36 @@ variable "bucket" {
   type    = string
   default = "pookflix"
 }
+variable "instance_type" {
+  # this app is network sensitive primarily, a t3.small should run it fine
+  default = "t3.micro"
+}
+variable "hostname" {
+  type    = string
+  default = "stream"
+}
+variable "instance_name" {
+  type = string
+  default = "streamlineinstance"
+}
 variable "aws_region" {
   default = "us-west-2"
 }
-
-variable "instance_type" {
-  # this app is network sensitive primarily, a t3.small should run it fine
-  default = "t3.small"
-}
-
 variable "instance_spot" {
   description = "Use a spot instance instead of an on-demand one"
   type        = bool
   default     = true
 }
-
 variable "instance_spot_price" {
   description = "The maximum spot price to accept when creating a spot instance"
   type        = string
   default     = ""
 }
-
 variable "instance_spot_block_duration" {
   description = "The block duration to request for the spot instance"
   type        = number
   default     = 0
 }
-
 variable "private_key_file" {
   description = "Whether to output a private key file for use by ssh"
   type        = bool
